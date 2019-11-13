b3744db33367299c172ada3c6f59139b0f542af6
Saif Ahmed <saifmdco@gmail.com>
2019-11-13 15:33:57 +0530

    modified

--- register/users/views.py
+++ register/users/views.py
@@ -15,7 +15,7 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f"New account created{username}")
             login(request, user)
-            return redirect("dash")
+            return redirect("index")
         else:
             for msg in form.error_messages:
                 messages.error(request, f"{msg}:{form.error_messages[msg]}")
@@ -40,7 +40,7 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 messages.success(request, f"You are logged in as {username}")
-                return redirect('dash')
+                return redirect('index')
 
             else:
                 messages.error(request, "Invalid username or password")
